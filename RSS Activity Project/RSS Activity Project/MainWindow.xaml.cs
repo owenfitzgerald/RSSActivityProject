cd9d701e4e2ac478a9f70c6d6b9c5321faa024f7
owenfitzgerald <opf2@duke.edu>
2019-07-02 13:37:21 -0400

    Initial Commit
    First project commit with general UI functionality.

---
+++ RSS Activity Project/RSS Activity Project/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RSS_Activity_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPress(object sender, RoutedEventArgs e)
+        {
+
+            var outputList = new List<string>();
+            outputList.Add("Metabolon");
+
+            outputBox.Text = "Reading File...";
+
+
+            var result = String.Join(",", outputList.ToArray());
+            outputBox.Text = result;
+        }
+    }
+}
